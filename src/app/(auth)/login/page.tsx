aeb35c0144886645a0e46da93d4ea152b31b848f
Theo de Sá <142926984+theo-mattsa@users.noreply.github.com>
2025-08-08 15:21:38 -0300

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -43,15 +43,11 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen mx-auto gap-4 md:gap-8 w-full max-w-4xl px-4 md:px-8">
       <div className="flex flex-col items-center gap-2">
-        <Swords className="w-12 h-12 md:w-16 md:h-16 transition-transform duration-1000 hover:rotate-360" />
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center">
-          Bem-vindo!
-        </h1>
         <Image
             src="/logo-completa.png"
             alt="GameVerse Logo"
-            width={160}
-            height={80}
+            width={180}
+            height={90}
           />
         <p className="text-sm md:text-base text-center text-gray-600">
           Entre com suas credenciais para acessar sua conta
